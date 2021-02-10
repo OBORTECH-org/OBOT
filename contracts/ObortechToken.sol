8549a704abd58e915f3e2c6cf44b0628e34d0e49
IntegralTeam K <repo@sfxdx.ru>
2021-02-10 23:34:50 +0300

    Create rules

--- contracts/ObortechToken.sol
+++ contracts/ObortechToken.sol
@@ -1,11 +1,42 @@
 pragma solidity >=0.6.0 <0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/GSN/Context.sol';
+import '@openzeppelin/contracts/access/AccessControl.sol';
 
-contract Token is ERC20 {
+
+contract ObertechToken is ERC20, AccessControl {
 
     constructor (string memory name_, string memory symbol_) public ERC20(name_,symbol_) {
-        //_mint(_msgSender(), 1000000 * 10 ** 18);
+        _mint(_msgSender(), 300000000 * 10 ** 18);
     }
+
+    bytes32 public constant OWNER = keccak256("OWNER");
+    
+    mapping (address => bool) private _burners;
+
+
+    function burn(address account, uint256 amount) external {
+       require(_burners[account] == true,"No rights");
+       _burn(account, amount);
+    }
+
+    function addBurner(address account) external  {
+        require(hasRole(OWNER, msg.sender),"No rights");
+        require(_burners[account] != true); 
+        _burners[account] = true;
+    }
+
+    function removeBurner(address account) external  {
+        require(hasRole(OWNER, msg.sender),"No rights");
+        require(_burners[account] != false);
+        _burners[account] = false;
+
+    }
+
+    function isBurner(address account)  external view returns (bool) {
+        require(_burners[account] == true,"No rights");
+        return true;
+    }
+
 
 }
--- test/ObertonToken.js
+++ test/ObertonToken.js
@@ -1,14 +1,34 @@
 const {
-    BN,
-    constants,
-    expectEvent,
-    expectRevert,
-    ether,
-    time,
-  } = require('@openzeppelin/test-helpers');
-  const chai = require('chai');
-  const { iteratee } = require('lodash');
-  const { isContractAddressInBloom } = require('web3-utils');
-  chai.use(require('chai-as-promised'));
-  const { expect } = chai;
-  
+  BN,
+  constants,
+  expectEvent,
+  expectRevert,
+  ether,
+  time,
+} = require('@openzeppelin/test-helpers');
+const chai = require('chai');
+const { iteratee } = require('lodash');
+const { isContractAddressInBloom } = require('web3-utils');
+chai.use(require('chai-as-promised'));
+const { expect } = chai;
+
+const ObertechToken = artifacts.require('ObertechToken');
+
+contract('ObertechToken',function(accounts){
+  const [owner,burner] = accounts;
+
+  const name = 'ObertechToken';
+  const symbol = 'OTKN';
+
+  beforeEach(async function(){
+    this.tokene = await ObertechToken.new(name,symbol);
+  });
+
+  it('Owner check', async function () {
+    await expectRevert(
+      this.tokene.addBurner(burner),
+      'No rights'
+    );
+  });
+
+});
