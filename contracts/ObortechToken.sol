044e4b8e9d4733ffde86262dc86ff6a9e8bdf004
IntegralTeam V <repo@sfxdx.ru>
2021-02-11 10:57:43 +0700

    fix isBurner

--- contracts/ObortechToken.sol
+++ contracts/ObortechToken.sol
@@ -7,11 +7,11 @@ import '@openzeppelin/contracts/access/AccessControl.sol';
 contract ObertechToken is ERC20, AccessControl {
 
     constructor (string memory name_, string memory symbol_) public ERC20(name_,symbol_) {
-        _mint(_msgSender(), 300000000 * 10 ** 18);
+        _mint(_msgSender(), 300_000_000 * 10 ** 18);
     }
 
     bytes32 public constant OWNER = keccak256("OWNER");
-    
+
     mapping (address => bool) private _burners;
 
 
@@ -22,7 +22,7 @@ contract ObertechToken is ERC20, AccessControl {
 
     function addBurner(address account) external  {
         require(hasRole(OWNER, msg.sender),"No rights");
-        require(_burners[account] != true); 
+        require(_burners[account] != true);
         _burners[account] = true;
     }
 
@@ -34,8 +34,7 @@ contract ObertechToken is ERC20, AccessControl {
     }
 
     function isBurner(address account)  external view returns (bool) {
-        require(_burners[account] == true,"No rights");
-        return true;
+        return _burners[account];
     }
 
 
