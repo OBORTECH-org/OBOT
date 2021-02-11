a79066210078bbcf7cc2d14056408c32bcec88fc
IntegralTeam V <repo@sfxdx.ru>
2021-02-11 11:01:18 +0700

    - _setupRole for contract deployer - refactoring

--- contracts/ObortechToken.sol
+++ contracts/ObortechToken.sol
@@ -1,19 +1,21 @@
 pragma solidity >=0.6.0 <0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import '@openzeppelin/contracts/GSN/Context.sol';
 import '@openzeppelin/contracts/access/AccessControl.sol';
 
 
 contract ObertechToken is ERC20, AccessControl {
-
-    constructor (string memory name_, string memory symbol_) public ERC20(name_,symbol_) {
-        _mint(_msgSender(), 300_000_000 * 10 ** 18);
-    }
-
     bytes32 public constant OWNER = keccak256("OWNER");
 
     mapping (address => bool) private _burners;
 
+    constructor (string memory name_, string memory symbol_) public ERC20(name_,symbol_) {
+        _mint(_msgSender(), 300_000_000 * 10 ** 18);
+        _setupRole(OWNER, _msgSender());
+    }
+
+    function isBurner(address account)  external view returns (bool) {
+        return _burners[account];
+    }
 
     function burn(address account, uint256 amount) external {
        require(_burners[account] == true,"No rights");
@@ -30,12 +32,5 @@ contract ObertechToken is ERC20, AccessControl {
         require(hasRole(OWNER, msg.sender),"No rights");
         require(_burners[account] != false);
         _burners[account] = false;
-
     }
-
-    function isBurner(address account)  external view returns (bool) {
-        return _burners[account];
-    }
-
-
 }
