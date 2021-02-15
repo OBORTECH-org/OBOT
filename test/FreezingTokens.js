96840c8b72917fc859e9a592d66e05612af5f34f
IntegralTeam K <repo@sfxdx.ru>
2021-02-15 13:19:11 +0300

    error in test FreezingTokens.js

--- test/FreezingTokens.js
+++ test/FreezingTokens.js
@@ -12,7 +12,27 @@ const {
   chai.use(require('chai-as-promised'));
   const { expect } = chai;
   
-  const ObertechToken = artifacts.require('FreezingTokens');
-  contract('ObertechToken',function(accounts) {
+  const ObertechToken = artifacts.require('ObertechToken');
+  const FreezingContract = artifacts.require('FreezingContract');
+  contract('FreezingContract',function(accounts) {
 
+    const [owner,burner,alice, founderAddress, managementAddress] = accounts;
+    const name = 'ObertechToken';
+    const symbol = 'OTKN';
+      
+    beforeEach(async function(){
+      this.token = await ObertechToken.new(name,symbol);
+      this.freezing_contract = await FreezingContract.new();
+      const startTime = await time.latest();
+      await this.token.approve(this.freezing_contract.address , ether('7000000') , { from: owner });
+      await this.freezing_contract.configure(this.token.address,startTime,founderAddress,managementAddress);
+
+    });
+
+    it('cheack founder address',async function (){
+      await expectRevert(this.freezing_contract.setFounderAddress( alice, { from: owner }),
+    'incorrect founderAddress'
+    );
+    });
+  
   });
