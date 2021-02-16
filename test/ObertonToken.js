300829d289aeb953bc85194390125138e4fadc57
IntegralTeam K <repo@sfxdx.ru>
2021-02-16 11:20:16 +0300

    error in grantRole

--- test/ObertonToken.js
+++ test/ObertonToken.js
@@ -22,56 +22,25 @@ contract('ObertechToken',function(accounts){
 
   beforeEach(async function(){
     this.token = await ObertechToken.new(name,symbol);
-  });
-
-  it('Add burner', async function () {
-    await this.token.addBurner(burner);
-    expect(await this.token.isBurner(burner),true);
-  });
-  
-  it('Double add burner', async function () {
-    await this.token.addBurner(burner);
-    await expectRevert(this.token.addBurner(burner),'User already exists');
-  });
-
-  it('Add burner not with owner', async function () {
-    await expectRevert( this.token.addBurner(burner, {from: alice}),
-    'No rights'
-    );
-  });
-
-
-  it('Remove burner', async function (){
-    await this.token.addBurner(burner);
-    expect(await this.token.isBurner(burner),true);
-    await this.token.removeBurner(burner);
-    expect(await this.token.isBurner(burner),false);
-  });
-
-  it('Double remove burner', async function (){
-    await this.token.addBurner(burner);
-    await this.token.removeBurner(burner);
-    await expectRevert (this.token.removeBurner(burner),'User already exists');
-  });
-
-  it('Remove burner not with owner', async function () {
-    await expectRevert(this.token.removeBurner(burner, {from: alice}),
-    'No rights'
-    );
+    
   });
 
   it('Burn', async function (){
-    await this.token.addBurner(burner);
+    //const burnerRole = await this.token.BURNER.call();
+    const ROLE = web3.utils.soliditySha3('BURNER');
+    console.log(ROLE);
+    
+    await this.token.grantRole(ROLE, burner, {form: owner});
     await this.token.transfer(burner, ether('10.0'), { from: owner });
     expect(await this.token.burn(burner,ether('1.0')));
   });
 
-  it('No rights to burn', async function (){
-    await this.token.transfer(burner, ether('10.0'), { from: owner });
-    await expectRevert(
-      this.token.burn(burner,ether('1.0')),
-      'No rights'
-      );
-  });
+  // it('No rights to burn', async function (){
+  //   await this.token.transfer(burner, ether('10.0'), { from: owner });
+  //   await expectRevert(
+  //     this.token.burn(burner,ether('1.0')),
+  //     'No rights'
+  //     );
+  // });
 
 });
